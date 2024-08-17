8dec6f505c3f51924a79b4685589e8728a60136a
Prabhjeev <prabhjeevnijjar@gmail.com>
2024-08-17 22:24:14 +0530

    aded suspense boundary

--- app/products/page.js
+++ app/products/page.js
@@ -4,6 +4,7 @@ import productData from '@/data/products.json';
 import Search from '../components/ProductsComp/SearchComp';
 import MetaTags from '../components/common/MetaTags';
 import { baseUrl } from '@/lib/constants';
+import { Suspense } from 'react';
 
 export const metadata = {
   title: 'Products page - Furniture',
@@ -15,26 +16,31 @@ export const metadata = {
 const productsPage = () => {
   return (
     <>
-      <MetaTags
-        title="Products page - Furniture"
-        description="This is the Products page of your website."
-        keywords="products, Ecomm website, Furniture"
-        author="Your Name"
-        ogImage="/images/homepage-image.jpg"
-        url={baseUrl + 'products'}
-      />
-      <div className="px-[2rem] md:px-[6rem]  2xl:px-[25rem] mb-10">
-        <div className="mx-auto flex items-center">
-          <img src={productBanner.src} className="hidden sm:block w-full"></img>
-          <img
-            src={productBannerSm.src}
-            className="block sm:hidden w-full"
-          ></img>
+      <Suspense>
+        <MetaTags
+          title="Products page - Furniture"
+          description="This is the Products page of your website."
+          keywords="products, Ecomm website, Furniture"
+          author="Your Name"
+          ogImage="/images/homepage-image.jpg"
+          url={baseUrl + 'products'}
+        />
+        <div className="px-[2rem] md:px-[6rem]  2xl:px-[25rem] mb-10">
+          <div className="mx-auto flex items-center">
+            <img
+              src={productBanner.src}
+              className="hidden sm:block w-full"
+            ></img>
+            <img
+              src={productBannerSm.src}
+              className="block sm:hidden w-full"
+            ></img>
+          </div>
+          <div className="mt-8 ">
+            <Search data={productData} />
+          </div>
         </div>
-        <div className="mt-8 ">
-          <Search data={productData} />
-        </div>
-      </div>
+      </Suspense>
     </>
   );
 };
